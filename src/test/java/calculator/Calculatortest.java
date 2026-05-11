4090b45f92e3a367122337f49f8126d20dd2c242
sooraj126 <soorajwork6@gmail.com>
2026-05-11 17:38:07 +1000

    Test Fail

--- src/test/java/calculator/Calculatortest.java
+++ src/test/java/calculator/Calculatortest.java
@@ -19,7 +19,7 @@ public class Calculatortest {
 
     @Test
     public void testMultiplication() {
-        assertEquals(30, calculator.multiply(5, 5));
+        assertEquals(45, calculator.multiply(5, 5));
     }
 
     @Test
